e16c39614c7c5053b4e1dc205796493412d02b82
Justin Appelgren <justinappelgren@gmail.com>
2021-01-22 15:40:01 -0600

    Setup reducers, store, provider, etc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const scoreReducer = (state = { feeling: 0, understanding: 0, support: 0 }, action) => {
+    switch (action.type) {
+        case 'SET_FEELING':
+            return state.feeling = action.payload
+
+        case 'SET_UNDERSTANDING':
+            return state.understanding = action.payload
+
+        case 'SET_SUPPORT':
+            return state.support = action.payload
+
+        default:
+            return state
+    }
+}
+
+const feedbackReducer = (state = '', action) => {
+    switch (action.type) {
+        case 'SET_FEEDBACK':
+            return action.payload
+
+        default:
+            return state
+    }
+}
+
+createStore(
+    combineReducers({
+        scoreReducer,
+        feedbackReducer
+    })
+)
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
