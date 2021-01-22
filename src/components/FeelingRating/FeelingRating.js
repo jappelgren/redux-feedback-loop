7bb645667a3511cd75ef6187e0e82154afb17863
Justin Appelgren <justinappelgren@gmail.com>
2021-01-22 16:43:43 -0600

    Added a little material ui rating element

--- src/components/FeelingRating/FeelingRating.js
+++ src/components/FeelingRating/FeelingRating.js
@@ -1,11 +1,69 @@
+import { useState } from 'react'
+import Rating from '@material-ui/lab/Rating';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+const customIcons = {
+    1: {
+        icon: <SentimentVeryDissatisfiedIcon />,
+        label: 'Very Dissatisfied',
+    },
+    2: {
+        icon: <SentimentDissatisfiedIcon />,
+        label: 'Dissatisfied',
+    },
+    3: {
+        icon: <SentimentSatisfiedIcon />,
+        label: 'Neutral',
+    },
+    4: {
+        icon: <SentimentSatisfiedAltIcon />,
+        label: 'Satisfied',
+    },
+    5: {
+        icon: <SentimentVerySatisfiedIcon />,
+        label: 'Very Satisfied',
+    },
+};
+
+function IconContainer(props) {
+    const { value, ...other } = props;
+    return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+IconContainer.propTypes = {
+    value: PropTypes.number.isRequired,
+};
+
+
 export default function FeelingRating() {
+    const [value, setValue] = useState(0);
+
     return (
         <div>
             <form>
                 <h2>How are you feeling today</h2>
-                <label>Feeling?</label>
-                <select name="support" id=""> Placeholder</select>
-                <button>Next</button>
+                <Box component="fieldset" mb={3} borderColor="transparent">
+                    <Typography component="legend">Feeling?</Typography>
+                    <Rating
+                        name="simple-controlled"
+                        getLabelText={(value) => customIcons[value].label}
+                        IconContainerComponent={IconContainer}
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                    />
+                </Box>
+
+                <button type="submit">Next</button>
             </form>
         </div>
     )
