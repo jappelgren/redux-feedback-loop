c11c1d8eb1661ba3c310db642e0e312e6e94d2e2
Justin Appelgren <justinappelgren@gmail.com>
2021-01-23 00:49:52 -0600

    Switched header to a material ui appbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,19 @@
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import FeedbackIcon from '@material-ui/icons/Feedback';
+
 export default function Header() {
     return (
-        <header className='App-header'>
-            <h1 className='App-title'>Feedback!</h1>
-            <h4>Don't forget it!</h4>
-        </header>
+        // <header className='App-header'>
+        //     <h1 className='App-title'>Feedback!</h1>
+        //     <h4>Don't forget it!</h4>
+        // </header>
+        <AppBar position="static" style={{ backgroundColor: "#23943f" }}>
+            <Toolbar>
+                <Typography variant="h2" >
+                    Feedback
+                </Typography>
+                <FeedbackIcon fontSize="large" />
+            </Toolbar>
+        </AppBar>
     )
 }
